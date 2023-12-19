b4db900442e003155720071285b8a131d20506f0
Dmytro <ihnatenkodmytro0@gmail.com>
2023-12-19 21:44:01 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import header from "./components/header";
+import footer from "./components/footer";
+
+
+ 
+function App() {
+  return (
+    <div className="wrapper">
+      <header />
+      <footer />
+    </div>
+  );
+}
+
+export default App;
